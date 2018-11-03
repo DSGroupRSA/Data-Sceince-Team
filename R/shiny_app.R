0bebda266d0201bbb8ac3e6ad16ab5da0281248e
Themba Gqaza <Gqztem001@myuct.ac.za>
2018-11-03 20:22:35 +0200

    updated the app

--- R/shiny_app.R
+++ R/shiny_app.R
@@ -12,6 +12,15 @@ library(data.table)
 library(DBI)
 library(RPostgreSQL)
 library(DT)
+#library(jsonlite)
+library()
+
+
+json_data <- rjson::fromJSON(file = "../secrets.json")
+drv <- dbDriver("PostgreSQL")
+conn <- dbConnect(drv, dbname = json_data$dbname, host = json_data$host, port = json_data$port, user = json_data$user, password=json_data$password)
+data <- dbGetQuery(conn, paste0("SELECT * FROM customers_table"))
+
 
 #----------------------------------
 #Input images
@@ -38,7 +47,7 @@ ui <- dashboardPage(skin = "blue",
 
   dashboardBody(
     uiOutput("menu1_content"),
-    uiOutput("menu2_content")
+    uiOutput("menu2_content",style = "height:500px; overflow-y: scroll;overflow-x: scroll;")
     )
 )
 
@@ -58,16 +67,15 @@ server <- function(input, output, session) {
                       else box(sidebar_menu[[2]])}
     })
 
+
+
       output$menu2_content <- renderUI({
                       sidebar_menu <- tstrsplit(input$sidebar_menu, "-")
-                      if (sidebar_menu[[1]] == "employee") box(title = "Sales", width = 12, height = 12,
-                        renderDataTable({
-                          drv <- dbDriver("PostgreSQL")
-                          conn <- dbConnect(drv, dbname = "test", host = "localhost", port = 5432, user = "themba")
-                          on.exit(dbDisconnect(conn), add = TRUE)
-                          dbGetQuery(conn, paste0("SELECT * FROM sales_table"))
-  }))
+                      if (sidebar_menu[[1]] == "employee") box(width = 15, height = 12, #title = "Sales", 
+                        datatable(data=data,options = list(columnDefs = list(list(className = 'dt-center', targets = 5)),
+                          pageLength = 5,lengthMenu = c(5, 10, 15, 20))
+  ))
     })
 }
-
+on.exit(dbDisconnect(conn), add = TRUE)
 shinyApp(ui, server)
