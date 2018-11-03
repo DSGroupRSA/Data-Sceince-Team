867b560ae266e0dcb3d9574825c847bfe02a1ca1
Themba Gqaza <Gqztem001@myuct.ac.za>
2018-11-03 20:25:34 +0200

    minor changes

--- python/mock_data_generator.py
+++ python/mock_data_generator.py
@@ -8,28 +8,30 @@ fake  =  Faker()
 provinces = ['Western Cape', 'Northen Cape', 'Eastern Cape', 'Gauteng', 'Limpopo', 'North West', 'Kwazulu Natal', 
              'Free State', 'Mpumalanga']
 com_channel = ['WhatsApp', 'call', 'email']
-table_columns = ['name','surname','cell_no','telephone_no','email','comm_channel', 'address','region','gender','reg_date','created_by_id']
-sex = ['Male','Female', 'Gay', 'Lesbian', 'Trans-Woman', 'Trans-Man']
+table_columns = ["firstname", "lastname", "gender", "email", "contact", "comm_channel","address", "postal_code", "city", "province", "reg_date"]
+sex = ['Male','Female', "Other"]
 
 
 clients = pd.DataFrame(columns=table_columns)
 
 
 for i in range(1,2):
-    name = str(fake.first_name())
-    surname = str(fake.last_name())
+    firstname = str(fake.first_name())
+    lastname = str(fake.last_name())
     gender = random.choice(sex)
-    cell_no = fake.msisdn()
-    telephone_no = str(fake.phone_number())
     email = str(fake.email())
+    contact = fake.msisdn()
     comm_channel = random.choice(com_channel)
-    address = {"street_no":random.choice([x for x in range(200,700)]), "street_name":str(fake.street_name()),
-                         "suburb":str(fake.city()), "postal_code":int(fake.postalcode())}
-    region = random.choice(provinces)
-    reg_date = str(fake.date_time())  #datetime.datetime.now()
-    created_bd_id = str(fake.user_name())
+    address = str(random.choice([x for x in range(200,700)]))+" "+str(fake.street_name())+" "+str(fake.city())
+    postal_code = int(fake.postalcode())
+    city = str(fake.city())
+    province = random.choice(provinces)
+    reg_date = str(fake.date_time())  
     
-    row = [name,surname,cell_no,telephone_no,email,comm_channel,str(address),region,gender,reg_date,created_bd_id]
+    row = [firstname, lastname, gender, email, contact, comm_channel, address, postal_code, city, province, reg_date]
     clients.loc[i] = row
 
-clients.to_csv('clients2.csv', sep=';', encoding='utf-8', index=False)
+#clients.to_csv('../data/customers.csv', sep=';', encoding='utf-8', index=False)
+print (address)
+
+
